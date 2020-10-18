cc0936f0a350a048d2f5cbce0a243002a4b1d685
r.galimov <samtakoy.first@gmail.com>
2020-10-18 12:55:47 +0300

    Di createComponent method

--- app/src/main/java/ru/samtakoy/listtest/app/Di.kt
+++ app/src/main/java/ru/samtakoy/listtest/app/Di.kt
@@ -1,8 +1,17 @@
 package ru.samtakoy.listtest.app
 
+import android.content.Context
 import ru.samtakoy.listtest.app.di.AppComponent
+import ru.samtakoy.listtest.app.di.DaggerAppComponent
 
 object Di {
 
     lateinit var appComponent: AppComponent
+
+    fun createComponent(context: Context){
+        appComponent = DaggerAppComponent.builder()
+            .setContext(context)
+            .build()
+    }
+
 }
--- app/src/main/java/ru/samtakoy/listtest/app/MyApp.kt
+++ app/src/main/java/ru/samtakoy/listtest/app/MyApp.kt
@@ -1,16 +1,12 @@
 package ru.samtakoy.listtest.app
 
 import android.app.Application
-import ru.samtakoy.listtest.app.Di
-import ru.samtakoy.listtest.app.di.DaggerAppComponent
 
 class MyApp : Application(){
 
     override fun onCreate() {
         super.onCreate()
 
-        Di.appComponent = DaggerAppComponent.builder()
-            .setContext(applicationContext)
-            .build()
+        Di.createComponent(applicationContext)
     }
 }
